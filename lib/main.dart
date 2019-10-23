c8b89b558a7fda872ed8c8f597cb6fba39801fef
Serageldin11 <serageldin.el-khodary@informatik.hs-fulda.de>
2019-10-23 15:52:40 +0200

    Homepage1

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,52 @@ class _KleineSeiteState extends State<KleineSeite> {
         title: Text("Erste App"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Image.network("https://www.hs-fulda.de/fileadmin/user_upload/Icons/hochschule_fulda_logo_opengraph.jpg"),
-          reiheMachen("Name", "Serag"),
-          reiheMachen("Alter", "20"),
-          reiheMachen("checkt", "nix"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Opacity(
+                opacity: 0.0,
+                child: Image.network(
+                    "https://www.hs-fulda.de/fileadmin/user_upload/Icons/hochschule_fulda_logo_opengraph.jpg"),
+              ),
+            ],
+          ),
+          Opacity(
+            opacity: 0.25,
+            child: Container(
+              color: Colors.amber[600],
+              width: 200.0,
+              height: 150.0,
+            ),
+          ),
+          AnimatedOpacity(
+            opacity: opacityLevel,
+            duration: Duration(seconds: 3),
+            child: Container(
+              color: Colors.pink[600],
+              width: 200.0,
+              height: 150.0,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Animation'),
+            onPressed: _changeOpacity,
+          ),
+          //reiheMachen("Name", "Serag"),
+          //reiheMachen("Alter", "20"),
+          //reiheMachen("checkt", "nix"),
         ],
       ),
     );
   }
 
+  double opacityLevel = 0.0;
+
+  void _changeOpacity() {
+    setState(() => opacityLevel = opacityLevel == 1 ? 0.0 : 1.0);
+  }
   Row reiheMachen(attribut, wert) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
